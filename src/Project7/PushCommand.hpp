5232eb16110fe5133d8f5ba5d5da2374313c2e6c
Adam Carter <mail@adam-carter.com>
2020-10-23 11:59:12 -0700

    implemented add and push constant in class exercise.

--- src/Project7/PushCommand.hpp
+++ src/Project7/PushCommand.hpp
@@ -21,6 +21,45 @@ public:
 	virtual string ToAsm()
 	{
 		ostringstream result{};
+
+		//convert command into equivalent assembly
+		switch (segment)
+		{
+		case Segment::Local:
+			break;
+		case Segment::Argument:
+			break;
+		case Segment::This:
+			break;
+		case Segment::That:
+			break;
+		case Segment::Temp:
+			break;
+		case Segment::Static:
+			break;
+		case Segment::Constant:
+
+			//load the constant 
+			result << "@" << arg1 << endl;
+			result << "D=A" << endl;
+
+			//store value on top of stack
+			result << "@SP" << endl;
+			result << "A=M" << endl;
+			result << "M=D" << endl;
+
+			break;
+		case Segment::Pointer:
+			break;
+		default:
+			break;
+		}
+
+		//increment stack pointer
+		result << "@SP" << endl;
+		result << "D=M+1" << endl;
+		result << "M=D" << endl;
+		
 		return result.str();
 	}
 };
